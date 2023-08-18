9ea6d8fe2e6241ac1f4842ce7f692663d2f34ad3
sujan khatri <sujansince2059@gmail.com>
2023-08-18 09:17:05 +0545

    complere

--- src/App.js
+++ src/App.js
@@ -7,27 +7,49 @@ const messages = [
   "Invest your new income 🤑",
 ]
 const App = () => {
-  let step = 1
+  const [step, setstep] = useState(1)
+  const [isOpen, setisOpen] = useState(true)
+
   return (
     <>
-      <div className="steps">
-        <div className="numbers">
-          <div className={`${step >= 1 ? "active" : ""}`}>1</div>
-          <div className={`${step >= 2 ? "active" : ""}`}>2</div>
-          <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+      <button
+        onClick={() => {
+          setisOpen(() => !isOpen)
+        }}
+        className="close"
+      >
+        &times;
+      </button>
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={`${step >= 1 ? "active" : ""}`}>1</div>
+            <div className={`${step >= 2 ? "active" : ""}`}>2</div>
+            <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+          </div>
+          <p className="message">
+            step {step}:{messages[step - 1]}
+          </p>
+          <div className="buttons">
+            <button
+              onClick={() => {
+                setstep(() => (step > 1 ? step - 1 : step))
+              }}
+              style={{ backgroundColor: "#7950f2", color: "white" }}
+            >
+              Previous
+            </button>
+            <button
+              onClick={() => {
+                setstep(() => (step < 3 ? step + 1 : step))
+              }}
+              style={{ backgroundColor: "#7950f2", color: "white" }}
+            >
+              Next
+            </button>
+          </div>
         </div>
-        <p className="message">
-          step {step}:{messages[step - 1]}
-        </p>
-        <div className="buttons">
-          <button style={{ backgroundColor: "#7950f2", color: "white" }}>
-            Previous
-          </button>
-          <button style={{ backgroundColor: "#7950f2", color: "white" }}>
-            Next
-          </button>
-        </div>
-      </div>
+      )}
     </>
   )
 }
